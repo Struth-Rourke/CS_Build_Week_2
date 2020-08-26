e287999888aed902948daf6e34d94fd5f6fad4a5
Rourke Struthers <struthers.rourke@gmail.com>
2020-08-26 13:31:06 -0400

    Tuesday

---
+++ tuesday.py
@@ -0,0 +1,33 @@
+# Given an array of integers, return indices of the two numbers such that they 
+# add up to a specific target.
+
+# You may assume that each input would have exactly one solution, and you may 
+# not use the same element twice.
+
+# EXAMPLE
+# Given nums = [2, 7, 11, 15], target = 9,
+
+# Because nums[0] + nums[1] = 2 + 7 = 9,
+# return [0, 1].
+
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        # instantiate an empty dictionary
+        required = {}
+        # loop through the indices of the nums
+        for i in range(len(nums)):
+            if target - nums[i] in required:
+                return [required[target - nums[i]], i]
+            else:
+                required[nums[i]] = i
+
+input_list = [2,8,12,15]
+ob1 = Solution()
+
+print(ob1.twoSum(input_list, 20))
